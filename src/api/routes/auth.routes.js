90a4004713e9035150803591097cd0e5ac70e54a
AbdulWasay2568 <abdulwasaydogar2002@gmail.com>
2025-08-22 07:25:30 +0500

    deployment

--- src/api/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const authRouter = (0, express_1.Router)();
-authRouter.post('/register', auth_controller_1.registerUserController);
-authRouter.post('/login', auth_controller_1.loginUserController);
-exports.default = authRouter;
